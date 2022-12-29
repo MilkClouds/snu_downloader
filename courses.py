0364887dc3c9754e073a39fafdfd37c140b81ff3
junukwon7 <junukwon7@gmail.com>
2022-12-29 13:53:39 +0900

    Changed login logic

--- courses.py
+++ courses.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 import dotenv, os, logging
 dotenv.load_dotenv(override = True)
 
-def get_lectures():
+def get_lectures(username: str, password: str):
     logging.info("Fetching lecture list...")
     with SB(headless=True) as sb:
         sb.open("https://myetl.snu.ac.kr")
-        sb.type("input#login_id", os.environ.get("username"))
-        sb.type("input#login_pwd", os.environ.get("password"))
-        sb.click('input[value="로그인"]')
+        sb.type("input#login_id", username)
+        sb.type("input#login_pwd", password)
+        sb.click('input[onclick="loginProc();"]')
         sb.open("https://myetl.snu.ac.kr/courses")
         trs = sb.find_elements("#my_courses_table > tbody > tr")
         lectures = [tr.find_element(By.CSS_SELECTOR, 'a').get_attribute('href').split("/")[-1] for tr in trs]
